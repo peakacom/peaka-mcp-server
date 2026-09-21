1a281c96233c7c95670395059cb9ae2ed8a7017b
Salih Altun <salih@peaka.com>
2026-09-21 13:52:43 +0300

    Update version

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ const authenticate =
 
 const server = new FastMCP<PeakaSession>({
   name: "Peaka",
-  version: "0.14.0",
+  version: "0.15.1",
   ...(mode === "httpStream" && {
     authenticate,
     health: {
